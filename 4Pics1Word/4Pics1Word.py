432f0ec8ef503203d4bb59a9af6e57dba4efb93b
HaiyangCui <cuiocean@gmail.com>
2017-02-27 21:41:18 +0100

    Better variable names, more comments

--- 4Pics1Word/4Pics1Word.py
+++ 4Pics1Word/4Pics1Word.py
@@ -1,25 +1,29 @@
+#!/usr/bin/python
 
-requiredLen = input("Please enter the number of letters: ")
-print("The English word you are looking for contains ", requiredLen, " letters")
+# Get the required number of letters
+lengthOfWord = input("How long is the word you're looking for: ")
 
-letterPool = input("Please enter the letters : ")
-print("The input letters are: ", letterPool)
+# Get the  
+inputLetters = input("Please enter the letters : ")
+
 
 # Load in the word list from a dictionary database
 with open('words.txt') as f:
-    words = f.readlines()
+	words = f.readlines()
 
-print("There are in total ", len(words), " words in the dictionary.")
+print("Possible answers for: ", inputLetters)
 
 for word in words:
-    wordToCheck = word[0:-1]
-    poolToCheck = letterPool
-    if len(wordToCheck) == int(requiredLen):
-        found = True
-        for elem in wordToCheck:
-            index = poolToCheck.find(elem)
-            if index == -1:
-                found = False
-            poolToCheck = poolToCheck[:index] + poolToCheck[index + 1:]
-        if found:
-            print(wordToCheck)
+	# Remove the escape character from the end of the word
+	wordToCheck = word[0:-1]
+	remainingLetters = inputLetters
+	if len(wordToCheck) == int(lengthOfWord):
+		found = True
+		for letter in wordToCheck:
+			index = remainingLetters.find(letter)
+			if index == -1:
+				found = False
+				break
+			remainingLetters = remainingLetters[:index] + remainingLetters[index + 1:]
+		if found:
+			print(wordToCheck)
